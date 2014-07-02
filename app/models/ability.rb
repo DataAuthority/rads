14c3b3539832ba03a7dfc416a58ef6b1a5575d45
Darin London <darin.london@duke.edu>
2014-07-02 16:44:32 -0400

    replaced more collects with join logic, still a couple that I could not change and pass tests???

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -17,12 +17,14 @@ class Ability
       can :destroy, Annotation, :creator_id => user.id
       can :manage, Record, :creator_id => user.id
       cannot :destroy, Record, :is_destroyed => true
-      can :read, Record, :id => user.projects.collect{|p| p.project_affiliated_records.collect{|m| m.record_id}}.flatten
+      can :read, Record, :project_affiliated_records => {:project => {:project_memberships => {:user_id => user.id}}}
       can :download, Record, :project_affiliated_records => {:project => {:project_memberships => {:user_id => user.id, :is_data_consumer => true}}}
       can :read, ProjectMembership, :project_id => user.projects.collect{|m| m.id}
-      can :manage, CartRecord, :user_id => user.id, :record_id => user.projects.collect{|p| p.project_affiliated_records.collect{|m| m.record_id}}.flatten + user.records.collect{|r| r.id }
-      can :update, Project, :id => user.project_memberships.where(is_data_producer: true).collect{|m| m.project_id}
- 
+#      can :read, ProjectMembership, :project => {:project_memberships => {:user_id => user.id}} # does not work, index wants success with empty list
+      can :manage, CartRecord, :user_id => user.id, :stored_record => {:project_affiliated_records => {:project => {:project_memberships => {:user_id => user.id}}}}
+      can :manage, CartRecord, :user_id => user.id, :stored_record => {:creator_id => user.id }
+      can :update, Project, :project_memberships => {:user_id => user.id, :is_data_producer => true}
+
       if user.is_administrator?
         can :manage, User, :type => nil
         can [:read, :edit, :update, :destroy, :switch_to], [RepositoryUser, CoreUser, ProjectUser]
@@ -39,16 +41,20 @@ class Ability
         can :read, Core
         can [:new, :create], [Core, Project]
         cannot :edit, Project
-        can [:edit, :update, :update_attributes], Project, :id => user.project_memberships.where(is_administrator: true).collect{|m| m.project_id}
+        can [:edit, :update, :update_attributes], Project, :project_memberships => {:user_id => user.id, :is_administrator => true}
         can [:destroy], ProjectAffiliatedRecord, :project => {:project_memberships => {:user_id => user.id, :is_administrator => true}}
-        can :switch_to, CoreUser, :core_id => user.cores.collect{|m| m.id}
-        can :switch_to, ProjectUser, :project_id => user.project_memberships.where(is_data_manager: true).collect{|m| m.project_id}
+        can :switch_to, CoreUser, :core => {:core_memberships => { :repository_user_id => user.id }}
+        can :switch_to, ProjectUser, :project => {:project_memberships => {:user_id => user.id, :is_data_manager => true}}
         can :manage, CoreMembership, :core_id => user.cores.collect{|m| m.id}
+#        can :manage, CoreMembership, :core => {:core_memberships => { :repository_user_id => user.id }} does not work, index wants success with empty list
         can [:edit, :update, :new, :create, :destroy], ProjectMembership, :project_id => user.project_memberships.where(is_administrator: true).collect{|m| m.project_id}.append(nil)
+        # this one is made difficult by the use of an authorize :create in the projects_controller
         cannot :destroy, CoreMembership, :repository_user_id => user.id
         cannot [:edit, :update, :destroy], ProjectMembership, :user_id => user.id
-        cannot :create, ProjectMembership, is_administrator: true, user_id: User.all.reject{|u| u.type == 'RepositoryUser'}.collect{|u| u.id}
-        cannot :create, ProjectMembership, is_data_manager: true, user_id: User.all.reject{|u| u.type == 'RepositoryUser'}.collect{|u| u.id}
+        cannot :create, ProjectMembership, is_administrator: true, user: {type: 'CoreUser'}
+        cannot :create, ProjectMembership, is_administrator: true, user: {type: 'ProjectUser'}
+        cannot :create, ProjectMembership, is_data_manager: true, user: {type: 'CoreUser'}
+        cannot :create, ProjectMembership, is_data_manager: true, user: {type: 'ProjectUser'}
       end
       if user.type == 'CoreUser'
         can :read, Core, id: user.core_id
