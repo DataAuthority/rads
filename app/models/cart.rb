d8b193523097226a43a7317884233c7c98ac1d92
Darin London <darin.london@duke.edu>
2014-07-24 15:51:50 -0400

    added annotation fields to cart model and carts _form ALL TESTS PASS

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,9 +1,9 @@
 class Cart
   include ActiveModel::Model
 
-  attr_accessor :action, :project_id
+  attr_accessor :action, :project_id, :context, :term
 
   def actions
-    %w{destroy_records affiliate_to_project}
+    %w{destroy_records affiliate_to_project annotate}
   end
 end
