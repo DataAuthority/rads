44a46b415e5d4589a5d8e113d92435551070a249
Darin London <darin.london@duke.edu>
2015-01-21 15:43:48 -0500

    refactored docker and crane to use shibboleth protected reverse proxy with the server
    server:
     -  does not have to install shibboleth
     - installs ruby and epel with a script
     - logs to STDOUT
    reverse_proxy
     - protects with shibboleth
     - passes necessary headers to /auth/shibboleth/callback

--- config/docker/development/install_ruby.sh
+++ config/docker/development/install_ruby.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-cd /root
+cd /root/installs
 wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.5.tar.gz
 tar -zxf ruby-2.1.5.tar.gz
-cd /root/ruby-2.1.5
+cd /root/installs/ruby-2.1.5
 ./configure --prefix=/usr/local --enable-shared --disable-install-doc
 make
 make install
-cd /root/ruby-2.1.5
+cd /root/installs/ruby-2.1.5
 /ext/readline
 /usr/local/bin/ruby extconf.rb
 make
 make install
-cd /root/ruby-2.1.5
+cd /root/installs/ruby-2.1.5
 /ext/zlib
 /usr/local/bin/ruby extconf.rb
 make
 make install
-cd /root/ruby-2.1.5
+cd /root/installs/ruby-2.1.5
 /ext/openssl
-export top_srcdir=/root/ruby-2.1.5
+export top_srcdir=/root/installs/ruby-2.1.5
 make
 make install
 cd /root
-rm -rf /root/ruby-2.1.5
-rm /root/ruby-2.1.5.tar.gz
+rm -rf /root/installs/ruby-2.1.5
+rm /root/installs/ruby-2.1.5.tar.gz
