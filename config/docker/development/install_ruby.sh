1e0b1d23c820af73ccb3e78a923c4ac2cc7e60d7
Darin London <darin.london@duke.edu>
2015-01-30 13:32:05 -0500

    uses nodejs as a javascript runtime, so no longer needs therubyracer install_ruby.sh uses local variables to store latest_ruby and latest_ruby_url

--- config/docker/development/install_ruby.sh
+++ config/docker/development/install_ruby.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 cd /root/installs
-wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.5.tar.gz
-tar -zxf ruby-2.1.5.tar.gz
-cd /root/installs/ruby-2.1.5
+latest_ruby="ruby-2.2.0"
+latest_ruby_url="http://cache.ruby-lang.org/pub/ruby/2.2/${latest_ruby}.tar.gz"
+wget ${latest_ruby_url}
+tar -zxf ${latest_ruby}.tar.gz
+cd /root/installs/${latest_ruby}
 ./configure --prefix=/usr/local --enable-shared --disable-install-doc
 make
 make install
-cd /root/installs/ruby-2.1.5
-/ext/readline
+cd /root/installs/${latest_ruby}/ext/readline
 /usr/local/bin/ruby extconf.rb
 make
 make install
-cd /root/installs/ruby-2.1.5
-/ext/zlib
+cd /root/installs/${latest_ruby}/ext/zlib
 /usr/local/bin/ruby extconf.rb
 make
 make install
-cd /root/installs/ruby-2.1.5
-/ext/openssl
-export top_srcdir=/root/installs/ruby-2.1.5
+cd /root/installs/${latest_ruby}/ext/openssl
+export top_srcdir=/root/installs/${latest_ruby}
 make
 make install
 cd /root
-rm -rf /root/installs/ruby-2.1.5
-rm /root/installs/ruby-2.1.5.tar.gz
+rm -rf /root/installs/${latest_ruby}
+rm /root/installs/${latest_ruby}.tar.gz
