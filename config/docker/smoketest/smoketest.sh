67bf99e8853ece12443824e478b9e00822a3a184
Darin London <darin.london@duke.edu>
2015-02-04 11:43:18 -0500

    smoketest Docker image
    This image is designed to be run with a GITREPO and optional GITBRANCH environment
    variable.  It pulls down the GITREPO and GITBRANCH (default is master)
    then runs the smoketest.sh script.
    This bundles, db:migrates, db:seeds, and rake tests the GITREPO
    
    Before you can build the image, it requires that you create
    
    config/docker/smoketest/smoketester_id_rsa
    config/docker/smoketest/smoketester_id_rsa.pub
    
    without a passphrase and register smokester_id_rsa.pub in your GITREPO keys
    
    Once you have these files run the following:
    $ docker build -t rads/smoketest config/docker/smoketest
    $ docker run -i -e 'GITREPO=git.repo.url.git' rads/smoketest

---
+++ config/docker/smoketest/smoketest.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ -z $GITREPO ]]; then
+  echo "make sure the GITREPO and optional GITBRANCH environment variables are set"
+  echo "  GITREPO is the git repo URL"
+  echo "  GITBRANCH is the BRANCH of the repo to test (default is master)"
+  exit 1
+fi
+if [[ -z $GITBRANCH ]]; then
+  export GITBRANCH=master
+fi
+
+mkdir -p ~/app
+cd ~/app
+git archive --remote=${GITREPO} ${GITBRANCH} | tar -xf -
+if [[ $? > 0 ]]; then
+  exit 1
+fi
+cat << DB > config/database.yml
+test:
+  adapter: sqlite3
+  database: db/test.sqlite3
+  pool: 5
+  timeout: 5000
+DB
+storage_root="${HOME}/rads"
+mkdir -p ${storage_root}
+echo 'Rads::Application.config.primary_storage_root = "'${storage_root}'"' > config/initializers/rads_storage.rb
+bundle config build.nokogiri --use-system-libraries
+bundle --path vendor/bundle
+bundle exec rake db:migrate
+bundle exec rake db:seed
+bundle exec rake test
